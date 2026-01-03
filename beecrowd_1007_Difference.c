eca398e8cb42f890e03cb471e2ddf80eab5282bc
Shakib Al Hassan <164641783+shakib-diu-436@users.noreply.github.com>
2026-01-03 23:00:27 +0600

    Create beecrowd_1007_Difference.c

---
+++ beecrowd_1007_Difference.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int A;
+    scanf("%d",&A);
+    int B;
+    scanf("%d",&B);
+    int C;
+    scanf("%d",&C);
+    int D;
+    scanf("%d",&D);
+    int X = (A*B)-(C*D);
+    printf("DIFERENCA = %d\n", X);
+    return 0;
+}
