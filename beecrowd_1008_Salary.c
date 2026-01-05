f5163e6521aa0fd890fe144e32c03612e305fb8d
Shakib Al Hassan <164641783+shakib-diu-436@users.noreply.github.com>
2026-01-05 22:45:34 +0600

    Create beecrowd_1008_Salary.c

---
+++ beecrowd_1008_Salary.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int A;
+    scanf("%d",&A);
+    int B;
+    scanf("%d",&B);
+    float C;
+    scanf("%f",&C);
+    float X =C*B;
+    printf("NUMBER = %d\n",A);
+    printf("SALARY = U$ %.2f\n", X);
+    return 0;
+}
